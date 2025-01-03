de4761cddc753701a4eed1707b7fdc6f1a61dfac
espadv69 <aitorjob69@gmail.com>
2025-01-03 15:47:38 +0100

    Add Facebook button functionality and enhance comments in 01-open.js

--- window/01-open.js
+++ window/01-open.js
@@ -4,10 +4,15 @@ youtube.addEventListener('click', () => { window.open('https://www.youtube.com')
 const google = document.querySelector('.google') // Select the google element (button) from the HTML
 google.addEventListener('click', () => { window.open('https://www.google.com', '_blank') }) // Open the google link when the google element (button) is clicked
 
+const facebookUrl = 'https://www.facebook.com' // Store the facebook URL in a variable
+const facebook = document.querySelector('.facebook') // Select the facebook element (button) from the HTML
+facebook.addEventListener('click', () => { window.open(facebookUrl, '_blank') }) // Open the facebook link when the facebook element (button) is clicked
+
 /** 
  * window.open() is a method that opens a new browser window or a new tab depending on the browser settings.
- * The first parameter is the URL of the page to open.
- * The second parameter is the name of the window. '_blank' is used to open the page in a new tab.
+ * The first parameter is the URL of the page to open. It is a required parameter.
+ * We can store the URL in a variable and pass the variable as the first parameter.
+ * The second parameter is the name of the window. '_blank' is used to open the page in a new tab. It is an optional parameter.
  * If the second parameter is not provided, the page will open in the same tab.
  * 
  * window.open('https://www.youtube.com') opens the youtube page in the same tab. Not always.
