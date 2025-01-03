eca5134830219f673b9d2447927054f5077cd311
espadv69 <aitorjob69@gmail.com>
2025-01-03 20:11:16 +0100

    Add showAlert function to demonstrate alert dialog usage

---
+++ window/04-alert.js
@@ -0,0 +1,17 @@
+// window.alert() or alert() is a method that displays an alert dialog with the optional specified content and an OK button.
+
+// It's not used at all in modern web development, but it's good to know about it.
+
+// This slows down the user experience and is not recommended to use.
+
+// Example
+
+function showAlert() {
+  alert("This is an alert box!");
+
+  console.log("This is after the alert box!");
+}
+
+// This will display an alert box with the message "This is an alert box!" and then log "This is after the alert box!" to the console.
+
+// This is beacuse the alert box is blocking the code execution until the user clicks the OK button. It means that the code after the alert box will not be executed until the user clicks the OK button.
