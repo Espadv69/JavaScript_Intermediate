5016221e6558edd3e6bc134ec2a635873ec54e8f
espadv69 <aitorjob69@gmail.com>
2025-01-03 19:58:13 +0100

    Implement stopLoading function to halt page loading and add documentation for window.stop() method

---
+++ window/03-stop.js
@@ -0,0 +1,11 @@
+function stopLoading() {
+  console.log('stop loading');
+  // stop loading
+  window.stop();
+}
+
+/**
+ * window.stop() method is used to stop the loading of the current page.
+ * 
+ * This method is used to stop the loading of the current page. It is equivalent to pressing the stop button in the browser.
+ */
