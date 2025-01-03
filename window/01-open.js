fe595ca19543a5aaac95ed9f1318128c83b8ede3
espadv69 <aitorjob69@gmail.com>
2025-01-03 15:43:11 +0100

    Add comments to clarify usage of window.open() in 01-open.js

--- window/01-open.js
+++ window/01-open.js
@@ -3,3 +3,13 @@ youtube.addEventListener('click', () => { window.open('https://www.youtube.com')
 
 const google = document.querySelector('.google') // Select the google element (button) from the HTML
 google.addEventListener('click', () => { window.open('https://www.google.com', '_blank') }) // Open the google link when the google element (button) is clicked
+
+/** 
+ * window.open() is a method that opens a new browser window or a new tab depending on the browser settings.
+ * The first parameter is the URL of the page to open.
+ * The second parameter is the name of the window. '_blank' is used to open the page in a new tab.
+ * If the second parameter is not provided, the page will open in the same tab.
+ * 
+ * window.open('https://www.youtube.com') opens the youtube page in the same tab. Not always.
+ * window.open('https://www.google.com', '_blank') opens the google page in a new tab.
+ **/
