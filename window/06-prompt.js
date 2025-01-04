f942831f77a2f8092171fbbf190c577e9f087a5d
espadv69 <aitorjob69@gmail.com>
2025-01-04 13:44:12 +0100

    Add showPrompt function to display a user input dialog

---
+++ window/06-prompt.js
@@ -0,0 +1,10 @@
+function showPrompt() {
+  const name = prompt('Enter your name') // prompt returns a string
+  alert(`Hello, ${name}`) // This returns Hello, <name> in an alert box
+}
+
+/**
+ * prompt() is a method that displays a dialog box that prompts the visitor for input.
+ * 
+ * It returns a string containing the text entered by the user, or null.
+ **/
