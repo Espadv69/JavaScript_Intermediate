bd05a85408c103cd6249d660c2d88f6eb1b798a4
espadv69 <aitorjob69@gmail.com>
2025-01-04 16:48:19 +0100

    Rename

---
+++ window/09-screen-left-top.js
@@ -0,0 +1,7 @@
+function showScreenLeftRight() {
+  let screenLeft = window.screenLeft
+  let screenRight = window.screenRight
+
+  console.log(`Screen left: ${screenLeft}`)
+  console.log(`Screen right: ${screenRight}`)
+}
