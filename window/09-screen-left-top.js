0e65c2c21b8a3652b59b27d0e0451b5242bee876
espadv69 <aitorjob69@gmail.com>
2025-01-04 16:50:43 +0100

    Rename function to showScreenLeftTop and update to log screen left and top positions

--- window/09-screen-left-top.js
+++ window/09-screen-left-top.js
@@ -1,7 +1,13 @@
-function showScreenLeftRight() {
-  let screenLeft = window.screenLeft
-  let screenRight = window.screenRight
+function showScreenLeftTop() {
+  let screenLeft = window.screenLeft // || window.screenX
+  let screenTop = window.screenTop // || window.screenY
 
-  console.log(`Screen left: ${screenLeft}`)
-  console.log(`Screen right: ${screenRight}`)
+  console.log(`Screen left: ${screenLeft}`) // Print the screen left
+  console.log(`Screen top: ${screenTop}`) // Print the screen top
 }
+
+/**
+ * The screenLeft and screenTop properties are used to get the screen position of the window.
+ * The screenLeft property returns the x-coordinate of the window relative to the screen.
+ * The screenTop property returns the y-coordinate of the window relative to the screen.
+ **/
