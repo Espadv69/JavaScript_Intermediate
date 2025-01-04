977048f84d991b3506dc0eaf3b6170ef570fa38c
espadv69 <aitorjob69@gmail.com>
2025-01-04 16:02:15 +0100

    Refactor confirmation dialog in 07-confirm.js to improve user feedback and add documentation

--- window/07-confirm.js
+++ window/07-confirm.js
@@ -1,9 +1,19 @@
 function showConfirm() {
-  let confirmOperation = window.confirm('Are you sure you want to close this page?')
+  // confirm() method displays a dialog box with a specified message, along with an OK and a Cancel button.
+  let confirm = window.confirm('do you want to continue with this action?')
 
-  if (confirmOperation) {
-    window.close()
+  if (confirm) {
+    alert('success, check the console')
+    console.log('Is the user confirmed?', confirm)
   } else {
-    alert('You chose not to close the page')
+    alert('success, check the console')
+    console.log('Is the user confirmed?', confirm)
   }
+
 }
+
+/**
+ * The confirm() method returns true if the user clicked "OK", and false otherwise.
+ * 
+ * The confirm() method is often used to ask if the user wants to do something, and can be used to ask for a confirmation before executing something.
+ **/
