29d4afbc0b2c10619db8f0e4a2494fb9546bf569
espadv69 <aitorjob69@gmail.com>
2025-01-06 20:03:25 +0100

    Add changeLocation function to navigate to a new URL

---
+++ window/16-location-assign.js
@@ -0,0 +1,9 @@
+function changeLocation() {
+  window.location.assign("https://www.google.com")
+}
+
+/**
+ * window.location.assign() method loads a new document.
+ * It replaces the current document with a new one.
+ * window.location.assign() is the same as setting the href property of the location object.
+ **/
