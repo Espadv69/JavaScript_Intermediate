47a5871a0ed0fc3e4095bb39c7abbca8eb59d339
espadv69 <aitorjob69@gmail.com>
2025-01-06 19:42:35 +0100

    Add functionality to display current page URL in 12-location-href.js

---
+++ window/12-location-href.js
@@ -0,0 +1,8 @@
+const href = window.location.href;
+document.write(href); // This returns the URL of the current page if this code has a URL.
+
+// Path: file///C:/Users/Owner/Documents/Projects/JavaScript-Playground/window/12-location-href.html for example
+
+// OR
+
+// Path: www.example.com if this code is on a website
